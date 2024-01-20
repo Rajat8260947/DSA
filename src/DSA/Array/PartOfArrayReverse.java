972fb272c74fc85ca0b2743b352ef21665511cf0
Rajat <131271415+Rajat8260947@users.noreply.github.com>
2024-01-20 18:12:21 +0530

    Update PartOfArrayReverse.java

--- src/DSA/Array/PartOfArrayReverse.java
+++ src/DSA/Array/PartOfArrayReverse.java
@@ -6,17 +6,19 @@ public class PartOfArrayReverse {
         int start_index=3;
         int end_index=7;
         reverse(arr,start_index,end_index);
-
+        System.out.println("After reverse");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
     }
     public static  void reverse(int[]arr,int sp,int ep){
         while(sp<ep){
             int temp =arr[sp];
             arr[sp] =arr[ep];
             arr[ep]=temp;
+            sp++;
+            cp--;
         }
-        System.out.println("After reverse");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
+       
 
         }
     }
